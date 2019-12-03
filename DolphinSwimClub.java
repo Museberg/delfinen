77ed16daca9b7baed9aa82c29f701346e4a06446
Museberg <Rasmus@falk-jensen.dk>
2019-12-03 10:49:06 +0100

    Fixed some classes

--- DolphinSwimClub.java
+++ DolphinSwimClub.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 public class DolphinSwimClub{
 	
-   public static void main(String[] args){
-      Member newMem = new Member("Rasmus Falk-Jensen", "Frederiksberg Bredegade 13B Frederiksberg 2000", 31953678, "28/05 1997", true, false);
-   
-      System.out.print(newMem);
-   }
 	public static void main(String[] args){
 		ArrayList<Trainer> trainerA = new ArrayList<Trainer>();
 		Trainer newTrainer = new Trainer("Uncle Jules", "Nørrebro Lige Der Over 2200", 12345678, "Team Jules", false);
